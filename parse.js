8e8f3f4b2ce889c395dc7ee3fa13fcd99343bfc6
Vi_ral <Varun.Rao@stonybrook.edu>
2016-11-19 13:12:26 -0500

    Parse.Js now retrieves dates from stony brook schedule website

--- parse.js
+++ parse.js
@@ -1,16 +1,29 @@
-$(document).ready(function(){
-	header("Access-Control-Allow-Origin: *");
+	function retrieve_data(){
+	var table=document.getElementById("id_of_table");
+	var rows=table.getElementsByTagName("tr");
 	var feed="http://stonybrookathletics.com/calendar.ashx/calendar.rss?sports_id=2";
-
 	$.get(feed, function(data){
+		var i=0;
 		$(data).find("description").each(function(){
 			var el=$(this);
-			console.log("title : "+el.text());
+			var text=el.text();
+			console.log(text);
+			if(text.includes("Tennis")){
+				consle.log(text);
+				i++;
+				var res=text.split(" ");
+				var row=table.insertRow(i);
+				var cell=row.insertCell(0);
 
+				for (j=0; j++; j<rows.length){
+					console.log(res);
+
+				}
+			}
 
 		});
 
 
 	});
+}
 
-});
